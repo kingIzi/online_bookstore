94e3c817d9041383b0fb6761d1605ad5319e15b1
Izi Kapampa <scott.izidore@gmail.com>
2022-01-28 06:24:49 +0300

    build

--- build-tests-Desktop_Qt_6_2_2_MSVC2019_64bit-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/scott/Documents/DEVELOPMENT/c++/QT6_2_0/online_bookstore/tests
-# Build directory: C:/Users/scott/Documents/DEVELOPMENT/c++/QT6_2_0/online_bookstore/build-tests-Desktop_Qt_6_2_2_MSVC2019_64bit-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(authentication "C:/Users/scott/Documents/DEVELOPMENT/c++/QT6_2_0/online_bookstore/build-tests-Desktop_Qt_6_2_2_MSVC2019_64bit-Debug/authentication.exe")
-set_tests_properties(authentication PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/scott/Documents/DEVELOPMENT/c++/QT6_2_0/online_bookstore/tests/CMakeLists.txt;46;add_test;C:/Users/scott/Documents/DEVELOPMENT/c++/QT6_2_0/online_bookstore/tests/CMakeLists.txt;0;")
